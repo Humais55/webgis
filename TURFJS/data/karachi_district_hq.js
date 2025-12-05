95788590decd5f0b7953fa98d1f58861a34e62c0
Humais55 <mohammadhumais6@gmail.com>
2025-12-05 09:00:53 +0500

    Karachi District

---
+++ TURFJS/data/karachi_district_hq.js
@@ -0,0 +1,18 @@
+{
+"type": "FeatureCollection",
+"name": "karachi_district_hq",
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KARACHI CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 67.0418921, 24.8533846 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KARACHI WEST" }, "geometry": { "type": "Point", "coordinates": [ 66.8307758, 24.8848451 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KARACHI CENTRAL" }, "geometry": { "type": "Point", "coordinates": [ 67.0515997, 24.9492191 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "MANORA CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 66.9763481, 24.7939395 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "CLIFTON CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 67.0770906, 24.7914151 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KORANGI CREEK CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 67.1229786, 24.7908205 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KORANGI" }, "geometry": { "type": "Point", "coordinates": [ 67.1209143, 24.8387298 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "MALIR" }, "geometry": { "type": "Point", "coordinates": [ 67.1869443, 24.8799145 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "MALIR CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 67.2161027, 24.9507813 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KARACHI EAST" }, "geometry": { "type": "Point", "coordinates": [ 67.072658, 24.8896694 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "FAISAL CANTONEMENT" }, "geometry": { "type": "Point", "coordinates": [ 67.1187356, 24.8862059 ] } },
+{ "type": "Feature", "properties": { "Id": 0, "DISTRICT": "KARACHI SOUTH" }, "geometry": { "type": "Point", "coordinates": [ 67.0259956, 24.8236057 ] } }
+]
+}
